e6b73663abfcb393c9f14cf4e5dcb4afef4baf7b
Asad Omar <asad.x.omar@gmail.com>
2021-11-24 18:28:11 +0000

    Step 2

--- part_1.py
+++ part_1.py
@@ -1,22 +1,34 @@
-selected_operator = input("Please selet an operator '*', '+', '-', or '/'?: ")
-first_number = int(input("Please enter your first number? "))
-second_number = int(input("Please enter your second number?: "))
+# selected_operator = input("Please selet an operator '*', '+', '-', or '/'?: ")
+# first_number = int(input("Please enter your first number? "))
+# second_number = int(input("Please enter your second number?: "))
 
 answer = 0
+sum_result = 0
 
-def calculate(operations, f_num, s_num):
+def calculate(operation, f_num, s_num):
     global answer
-    if operations == "+":
+    if operation == "+":
         answer = f_num + s_num
-    elif operations == "-":
+    elif operation == "-":
         answer = f_num - s_num
-    elif operations == "*":
+    elif operation == "x":
         answer = f_num * s_num
     else:
         answer = f_num / s_num
     return answer
 
-print(calculate(operations=selected_operator,f_num=first_number,s_num=second_number))
+# print(calculate(operation=selected_operator,f_num=first_number,s_num=second_number))
+
+filePath = r"C:\Users\asado\DevOps\Module 1\Exercises\Workshop Part 1\calc.txt"
+with open(filePath, 'r', encoding='utf-8') as f:
+    calc_list = f.read()
+    for line in calc_list.splitlines(True):
+        calc_line = line.split()
+        selected_operator = calc_line[1]
+        first_number = int(calc_line[2])
+        second_number = int(calc_line[3])
+        calculate(operation=selected_operator,f_num=first_number,s_num=second_number)
+        sum_result += answer
 
 
-
+    print(sum_result)
