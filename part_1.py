4b98d29ae682f45d0e409e5b47c40c8078ec6d0c
Asad Omar <asad.x.omar@gmail.com>
2021-11-25 16:48:39 +0000

    Step 4

--- part_1.py
+++ part_1.py
@@ -2,6 +2,9 @@
 # first_number = int(input("Please enter your first number? "))
 # second_number = int(input("Please enter your second number?: "))
 
+from os import replace
+
+
 answer = 0
 sum_result = 0
 
@@ -19,16 +22,48 @@ def calculate(operation, f_num, s_num):
     return int(answer)
 
 
-def process_goto(process_line):
-    if process_line[1] == "calc":
-        operator = process_line[2]
-        first_number = int(process_line[3])
-        second_number = int(process_line[4])
+def process_goto(line_to_process):
+    if line_to_process[1] == "calc":
+        operator = line_to_process[2]
+        first_number = int(line_to_process[3])
+        second_number = int(line_to_process[4])
         calculate(operation=operator,f_num = first_number,s_num =second_number)
         return int(answer)
     
     else:
-        return int(process_line[1])
+        return int(line_to_process[1])
+
+
+def process_remove(line_to_process, current_line, file_line):
+    line_to_remove = int(line_to_process[1])
+    if line_to_remove <= current_line:
+        line_to_process_next = current_line
+    else:
+        line_to_process_next = current_line + 1
+    file_line.pop(line_to_remove)
+    return line_to_process_next
+
+
+def process_replace(line_to_process, current_line, file_line):
+    line_to_repace = int(line_to_process[1])
+    line_to_copy = int(line_to_process[2])
+    copied_line = file_line[line_to_copy - 1]
+    file_line.pop(line_to_repace - 1)
+    file_line.insert(line_to_repace -1, copied_line)
+    return current_line + 1
+
+def process_all(line_to_process, current_line, file_line):
+    instructions = line_to_process[0]
+    if instructions == "goto":
+        return process_goto(line_to_process)
+    elif instructions == "remove":
+        return process_remove(line_to_process, current_line, file_line)
+    elif instructions == "remove":
+        return process_remove(line_to_process, current_line, file_line)
+    else:
+        raise Exception("Unexpected Instruction!")
+
+
 
 
 # # print(calculate(operation=selected_operator,f_num=first_number,s_num=second_number))
@@ -45,22 +80,22 @@ def process_goto(process_line):
 #         sum_result += answer
 #     print(sum_result)
 
-filePath = r"C:\Users\asado\DevOps\Module 1\Exercises\Workshop Part 1\goto.txt"
+filePath = r"C:\Users\asado\DevOps\Module 1\Exercises\Workshop Part 1\remove.txt"
 with open(filePath, "r", encoding='utf-8') as f:
-    line = f.read().splitlines(True)
+    file_line = f.read().splitlines(True)
     line_hits = set()
     line_number = 1
     while True:
-        current_line = line[line_number - 1]
+        current_line = file_line[line_number - 1]
         if current_line in line_hits:
             break
-        line_togo = current_line.split()
-        new_line = process_goto(line_togo)
+        line_to_process = current_line.split()
+        new_line = process_all(line_to_process, line_number, file_line)
         print(current_line)
         print(new_line)
         line_hits.add(current_line)
         line_number = new_line
-    print(f"Answer: Line {current_line}{line_number}")
+    print(f"Answer: Line {line_number} {current_line}")
 
 
 
