ef13b76343f1b549650fce511dbe7ca1c530eeab
Asad Omar <asad.omar@yahoo.co.uk>
2021-11-29 15:50:11 +0000

    Part 3 Step 3

--- app/app.py
+++ app/app.py
@@ -1,13 +1,25 @@
 from flask import Flask, render_template
 
-films = ["Flubber", "Jumanji", "Aladdin"]
+list_of_films = []
+id = 0
+
+filePath = r"C:\Users\asado\DevOps\Module 1\Exercises\Workshop Part 1\app\list_of_films.csv"
+with open(filePath, "r", encoding='utf-8') as f:
+    films = f.read()
+    for film in films.splitlines():
+        line_film = film.split(",")
+        id += 1
+        film_rating = {"id": id, "title": line_film[0], "rating": line_film[1]}
+        list_of_films.append(film_rating)
+   
+    print(list_of_films)
+
 message = "List of Films"
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html", message=message, films=films);
+    return render_template("index.html", message=message, list_of_films=list_of_films);
 
 
-print(films)
